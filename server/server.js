1b50bd861712fb278f577610199ebb1cc9f56d4c
Tyler <tbuchhei@alumni.nd.edu>
2018-02-22 20:17:39 -0600

    Setup basic server with static page and deploy

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000
+
+app.use(express.static(publicPath));
+
+
+// Run
+
+app.listen(3000, () => console.log(`Listening on port ${port}`));
