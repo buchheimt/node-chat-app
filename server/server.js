79a838c439624d8f0be69a3f1876b5a70cee296a
Tyler <tbuchhei@alumni.nd.edu>
2018-02-23 16:38:10 -0600

    Handle new users with greeting and alerts to other users

--- server/server.js
+++ server/server.js
@@ -15,13 +15,29 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to Super Chat!',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'A new user has joined Super Chat!',
+    createdAt: new Date().getTime()
+  });
+  
   socket.on('createMessage', message => {
     console.log(`New message from ${message.from}:`, message.text);
 
-    io.emit('newMessage', {
-      ...message,
-      createdAt: new Date().getTime()
-    });
+    // io.emit('newMessage', {
+    //   ...message,
+    //   createdAt: new Date().getTime()
+    // });
+    // socket.broadcast.emit('newMessage', {
+    //   ...message,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
 
