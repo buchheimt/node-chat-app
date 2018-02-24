e4be642eda2d42268f8b7eba15c84ab51e05d05a
Tyler <tbuchhei@alumni.nd.edu>
2018-02-24 15:26:13 -0600

    Add basic form and display for messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,27 @@
 const socket = io();
 
-    socket.on('connect', function() {
-      console.log('Connected to server');
-    });
+socket.on('connect', function() {
+  console.log('Connected to server');
+});
 
-    socket.on('newMessage', function(message) {
-      console.log(`New message from ${message.from}:`, message.text);
-    });
+socket.on('disconnect', function() {
+  console.log('Disconnected from server');
+});
 
-    socket.emit('createMessage', {
-      from: 'Tyler',
-      text: 'Hi'
-    }, function(data) {
-      console.log('Got it:', data);
-    });
+socket.on('newMessage', function(message) {
+  console.log(`New message from ${message.from}:`, message.text);
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  $('#messages').append(li);
+});
 
 
-    socket.on('disconnect', function() {
-      console.log('Disconnected from server');
-    });
-
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function() {
     
+  });
+});
