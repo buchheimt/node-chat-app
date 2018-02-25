0714a24038e739c981b5ee0fc600833649de19e2
Tyler <tbuchhei@alumni.nd.edu>
2018-02-25 10:38:15 -0600

    Use Mustache.js for messages and refactor client-side to ES6

--- public/js/index.js
+++ public/js/index.js
@@ -1,47 +1,55 @@
 const socket = io();
 
-socket.on('connect', function() {
+socket.on('connect', () => {
   console.log('Connected to server');
 });
 
-socket.on('disconnect', function() {
+socket.on('disconnect', () => {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(message) {
-  var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = $('<li></li>');
-  li.text(`${message.from} - ${formattedTime}: ${message.text}`);
-  $('#messages').append(li);
+socket.on('newMessage', message => {
+  const formattedTime = moment(message.createdAt).format('h:mm a');
+
+  const template = $('#message-template').html();
+  const html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  $('#messages').append(html);
 });
 
-socket.on('newLocationMessage', function(message) {
-  var li = $('<li></li>');
-  var a = $('<a target="_blank">My current location</a>');
-  var formattedTime = moment(message.createdAt).format('h:mm a');
+socket.on('newLocationMessage', message => {
+  const formattedTime = moment(message.createdAt).format('h:mm a');
 
-  li.text(`${message.from} - ${formattedTime}:`);
-  a.attr('href', message.url);
-  li.append(a);
-  $('#messages').append(li);
+  const template = $('#location-message-template').html();
+  const html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  $('#messages').append(html);
 });
 
 
-$('#message-form').on('submit', function(e) {
+$('#message-form').on('submit', e => {
   e.preventDefault();
 
-  var $messageInput = $('[name=message]');
+  const $messageInput = $('[name=message]');
 
   socket.emit('createMessage', {
     from: $('[name=username]').val(),
     text: $messageInput.val()
-  }, function() {
+  }, () => {
     $messageInput.val('');
   });
 });
 
-var $locationButton = $('#send-location');
-$locationButton.on('click', function() {
+const $locationButton = $('#send-location');
+$locationButton.on('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser');
   }
@@ -54,7 +62,7 @@ $locationButton.on('click', function() {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
-  }, function() {
+  }, () => {
     alert('Unable to fetch location');
     $locationButton.removeAttr('disabled').text('Send Location');
   });
